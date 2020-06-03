0ca53dba04e6c479e9d7e96166542ec4937a6e8b
Kochi <c011610813@edu.teu.ac.jp>
2020-06-03 09:27:23 +0900

    各種エラー修正、未対応言語設定時もダイアログ表示するよう変更

--- ConfigManager.py
+++ ConfigManager.py
@@ -41,6 +41,26 @@ class ConfigManager(configparser.ConfigParser):
 			self.add_section(key)
 			return self.__getitem__(key)
 
+	def getstring(self,section,key,default="",selection=None,*, raw=False, vars=None,fallback=None):
+		if type(selection) not in (set,tuple,list):
+			raise TypeError("selection must be set, list or tuple")
+		ret=self.__getitem__(section)[key]
+		if ret=="":
+			if default=="":
+				self[section][key]=""
+				return ""
+			else:
+				self.log.debug("add default value.  at section "+section+", key "+key)
+				self[section][key]=default
+				ret=default
+				if selection==None:return ret
+
+		if ret not in selection:
+			self.log.debug("value "+ret+" not in selection.  at section "+section+", key "+key)
+			self[section][key]=default
+			ret=default
+		return ret
+
 	def getint(self,section,key,default=0,min=None,max=None):
 		if type(default)!=int:
 			raise ValueError("default value must be int")
