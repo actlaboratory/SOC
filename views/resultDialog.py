fe96dcb1e276c1fccbf7312af04c185ab581634d
Kochi <c011610813@edu.teu.ac.jp>
2021-05-27 09:10:52 +0900

    タブコントロールを設置

--- views/resultDialog.py
+++ views/resultDialog.py
@@ -22,7 +22,14 @@ class Dialog(BaseDialog):
 	def InstallControls(self):
 		"""いろんなwidgetを設置する。"""
 		self.creator=views.ViewCreator.ViewCreator(self.viewMode,self.panel,self.sizer,wx.VERTICAL,20,style=wx.ALL,margin=20)
-		self.resEdit, dummy = self.creator.inputbox(_("認識結果"), defaultValue = self.text, style = wx.TE_READONLY|wx.TE_MULTILINE|wx.TE_DONTWRAP, )
+		tabCtrl = self.creator.tabCtrl(_("ページ切替"),sizerFlag=wx.ALL|wx.EXPAND, proportion=1, margin=5):
+
+		page = views.ViewCreator.ViewCreator(self.viewMode,tabCtrl,None,wx.VERTICAL,label=_("進行状況"),style=wx.ALL|wx.EXPAND,proportion=1,margin=20)
+
+
+		page = views.ViewCreator.ViewCreator(self.viewMode,tabCtrl,None,wx.VERTICAL,label=_("認識結果"),style=wx.ALL|wx.EXPAND,proportion=1,margin=20)
+		self.resEdit, dummy = creator.inputbox(_("認識結果"), defaultValue = self.text, style = wx.TE_READONLY|wx.TE_MULTILINE|wx.TE_DONTWRAP, )
+
 		self.button = self.creator.okbutton(_("閉じる"))
 
 	def getData(self):
