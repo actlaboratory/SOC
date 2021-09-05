6e42bb30dd7f7aa8a2770e6a917c920f9c3c6655
guredora <contact@guredora.com>
2021-09-05 17:44:30 +0900

    taskの機能追加

--- task.py
+++ task.py
@@ -1,10 +1,46 @@
 from enum import IntFlag, auto
+import threading
+from logging import getLogger
+import constants
+from sources.constants import sourceStatus
+from engines.constants import engineStatus
+import time
 
-class task:
+class task(threading.Thread):
 	def __init__(self, source, engine):
+		super().__init__()
 		self.source = source
 		self.engine = engine
 		self.jobs = []
+		self.log = getLogger("%s.task" % (constants.APP_NAME))
+		self.log.debug("initialized")
+
+	def run(self):
+		self.raiseStatusFlag(taskStatus.STARTED)
+		self.log.debug("initializing modules...")
+		self.source.initialize()
+		self.source.start()
+		self.engine.setCallbackOnAfterRecognize(self.onAfterRecognize)
+		self.engine.start()
+		self.log.info("Ocr started")
+		time.sleep(1)
+		while True:
+			if not self.source.getStatus() & sourceStatus.RUNNING:
+				if self.source.empty():
+					break
+			if self.source.empty():
+				time.sleep(0.1)
+				continue
+			job = self.source.getNextJob()
+			self.jobs.append(job)
+			self.log.debug("job received")
+			self.engine.put(job)
+			time.sleep(0.01)
+		self.engine.notifyStopSource()
+		while not self.engine.getStatus() & engineStatus.FINISHED:
+			time.sleep(0.01)
+		self.log.debug("Ocr done!")
+		self.raiseStatusFlag(taskStatus.DONE)
 
 	def getJobs(self):
 		return self.jobs
@@ -14,6 +50,17 @@ class task:
 
 	def getSourceStatus(self):
 		return self.source.getStatus()
+
+	def raiseStatusFlag(self, flag):
+		assert isinstance(flag, taskStatus)
+		self.status |= flag
+
+	def lowerStatusFlag(self, flag):
+		assert isinstance(flag, taskStatus)
+		self.status &= -1-flag
+
+	def getStatus(self):
+		return self.status
 
 class taskStatus(IntFlag):
 	STARTED = auto()
