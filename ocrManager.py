9a55657e1fafe5523f185f764929edffeb386b79
guredora <contact@guredora.com>
2021-09-06 16:42:29 +0900

    OcrManagerのログを強化

--- ocrManager.py
+++ ocrManager.py
@@ -15,13 +15,17 @@ class manager(threading.Thread):
 		self.needStop = False
 		self.running = False
 		self.log = getLogger("%s.manager" % (constants.APP_NAME))
+		self.log.info("initialized")
 
 	def run(self):
+		self.log.info("manager started")
 		self.running = True
 		while self.needStop:
 			time.sleep(0.01)
 			task = self.taskQueue.get(True)
+			self.log.debug("got task-%d" % task.getID())
 			self.processTask(task)
+		self.log.info("stoped")
 		self.running = False
 
 	def processTask(self, task):
@@ -29,9 +33,11 @@ class manager(threading.Thread):
 		while task.getStatus() & taskStatus.DONE:
 			time.sleep(0.01)
 			continue
+		self.log.info("task processed")
 		return
 
 	def addTask(self, task):
+		self.log.debug("added task-%d" % task.getID())
 		self.tasks.append(task)
 		self.taskQueue.put(task)
 
