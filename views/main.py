8d4eec34b65f43e2d6db98e671ee7a7e572ea7d5
Kazto Kitabatake <kitabatake1013@outlook.jp>
2021-09-07 13:43:12 +0900

    メインビューを実装（まだまともに使えません）

--- views/main.py
+++ views/main.py
@@ -5,6 +5,7 @@
 #Copyright (C) 2020 guredora <contact@guredora.com>
 
 import ctypes
+import copy
 import logging
 import os
 import pathlib
@@ -56,6 +57,115 @@ class MainView(BaseView):
 		)
 
 		self.InstallMenuEvent(Menu(self.identifier),self.events.OnMenuSelect)
+
+		self.initialized = False
+		self.jobs = []
+		self.map = {}
+		tabCtrl = self.creator.tabCtrl(_("ページ切替"),sizerFlag=wx.ALL|wx.EXPAND, proportion=1, margin=5)
+
+		page = views.ViewCreator.ViewCreator(self.viewMode,tabCtrl,None,wx.VERTICAL,label=_("進行状況"),style=wx.ALL|wx.EXPAND,proportion=1,margin=20)
+
+
+		page = views.ViewCreator.ViewCreator(self.viewMode,tabCtrl,None,wx.VERTICAL,label=_("認識結果"),style=wx.ALL|wx.EXPAND,proportion=1,margin=20)
+		self.tree, dummy = page.treeCtrl(_("認識済みファイル"), self.itemSelected)
+		self.tree.Bind(wx.EVT_CONTEXT_MENU, self.onContextMenu)
+		self.treeIdentifier = "resultTree"
+		self.menu.keymap.Set(self.treeIdentifier, self.tree)
+		self.text, dummy = page.inputbox(_("認識結果"), style=wx.TE_READONLY|wx.TE_MULTILINE)
+		self.update()
+		self.creator.okbutton(_("閉じる"), self.onClose)
+
+		self.evtHandler = wx.EvtHandler()
+		self.evtHandler.Bind(wx.EVT_TIMER, self.onTimerEvent)
+		self.timer = wx.Timer(self.evtHandler)
+		self.timer.Start(300)
+
+		self.initialized = True
+
+	def update(self):
+		jobs = self.getAllJobs()
+		if not self.initialized:
+			# 初回のみ実行
+			root = self.tree.AddRoot(_("（全て）"))
+			ret = jobs
+			self.map[root] = {"cursor": 0, "text": self.getAllText()}
+		else:
+			# タイマーでのみ実行
+			root = self.tree.GetRootItem()
+			ret = jobs[len(self.jobs):]
+			self.map[root]["text"] = self.getAllText()
+		for job in ret:
+			item1 = self.tree.AppendItem(root, job.getFileName())
+			if item1 in self.map:
+				self.map[item1]["text"] = job.getAllItemText()
+			else:
+				self.map[item1] = {"cursor": 0, "text": job.getAllItemText()}
+			items = job.getItems()
+			if len(items) < 2:
+				continue
+			for item in items:
+				item2 = self.tree.AppendItem(item1, item.getFileName())
+				if item2 in self.map:
+					self.map[item2]["text"] = item.getText()
+				else:
+					self.map[item2] = {"cursor": 0, "text": item.getText()}
+		self.updateText()
+		self.jobs = copy.deepcopy(jobs)
+
+	def getAllJobs(self):
+		jobs = []
+		for task in globalVars.manager.getTasks():
+			for job in task.getProcessedJobs():
+				jobs.append(job)
+		return jobs
+
+	def getAllText(self):
+		ret = []
+		for i in self.getAllJobs():
+			ret.append(self.getJobText(i))
+		return "".join(ret)
+
+	def getJobText(self, job):
+		ret = []
+		for i in job.getItems():
+			ret.append(i.getText())
+		return "".join(ret)
+
+	def updateText(self):
+		# 「全て」を選択時、新しく認識されたテキストを追加する
+		root = self.tree.GetRootItem()
+		if self.tree.GetFocusedItem() != root:
+			return
+		new = self.map[root]["text"][len(self.text.GetValue()):]
+		cursor = self.text.GetSelection()
+		self.text.AppendText(new)
+		self.text.SetSelection(cursor[0], cursor[1])
+
+	def itemSelected(self, event):
+		prev = event.GetOldItem()
+		cursor = self.text.GetInsertionPoint()
+		if prev.GetID() is not None:
+			self.map[prev]["cursor"] = cursor
+		self.text.Clear()
+		focus = event.GetItem()
+		self.text.SetValue(self.map[focus]["text"])
+		self.text.SetInsertionPoint(self.map[focus]["cursor"])
+
+	def onTimerEvent(self, event):
+		self.update()
+
+	def onClose(self, event):
+		self.timer.Stop()
+		event.Skip()
+
+	def onContextMenu(self, event):
+		menu = wx.Menu()
+		menu.Bind(wx.EVT_MENU, globalVars.app.hMainView.events.OnMenuSelect)
+		baseMenu = BaseMenu(self.treeIdentifier)
+		baseMenu.RegisterMenuCommand(menu, [
+			"COPY_TEXT",
+		])
+		self.tree.PopupMenu(menu, event)
 
 class Menu(BaseMenu):
 	def Apply(self,target):
