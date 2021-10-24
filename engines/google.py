f508dc4fb6947ef57958bd0006bc0a5745fff714
guredora <contact@guredora.com>
2021-10-24 18:03:08 +0900

    googleのengine初期化時のみに、serviceのbuildを行うように変更

--- engines/google.py
+++ engines/google.py
@@ -21,12 +21,12 @@ class googleEngine(engineBase):
 		if not self.credential.isOK():
 			self.log.error("credential not authorized")
 		self.credential.Authorize()
+		self.service = discovery.build("drive", "v3", credentials=self.credential.credential)
 
 	def getSupportedFormats(self):
 		return constants.FORMAT_JPEG | constants.FORMAT_PNG | constants.FORMAT_GIF|constants.FORMAT_PDF_IMAGE
 
 	def _recognize(self, item):
-		service = discovery.build("drive", "v3", credentials=self.credential.credential)
 		with open(item.getPath(), mode = "rb") as f:
 			self.log.info("uploading...")
 			media_body = MediaIoBaseUpload(f, mimetype="application/vnd.google-apps.document", resumable=True)
@@ -34,19 +34,19 @@ class googleEngine(engineBase):
 				"name": os.path.basename(item.getPath()),
 				"mimeType":"application/vnd.google-apps.document"
 			}
-			file = service.files().create(
+			file = self.service.files().create(
 				body = req_body,
 				media_body = media_body,
 				ocrLanguage = "ja",
 				fields="id"
 			).execute()
 		ID = file.get("id")
-		request = service.files().export_media(fileId=ID, mimeType = "text/plain")
+		request = self.service.files().export_media(fileId=ID, mimeType = "text/plain")
 		stream = io.BytesIO()
 		downloader = MediaIoBaseDownload(stream, request)
 		done = False
 		while done is False:
 			status, done = downloader.next_chunk()
-		service.files().delete(fileId=ID).execute()
+		self.service.files().delete(fileId=ID).execute()
 		item.setText(stream.getvalue().decode("utf-8"))
 
